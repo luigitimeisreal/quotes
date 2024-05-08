fcedb06595a51dda4036445661c9b485ff30798b
Antonio Riesco <antoniors2003@gmail.com>
2024-05-08 12:23:34 +0200

    Version 1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+// import * as jsonQuotes from '../../assets/data/quotes.json'
+import data from '../../assets/data/quotes.json';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  quote: string = "";
+  autor: string = "";
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    // this.dataService.getData().subscribe(response => {
+    //   console.log(response);
+    // })
+    this.generateQuote();
+
+
+  }
+
+  generateQuote() {
+    // Generate a random number
+    let randomNum = Math.floor(Math.random() * (data.length - 1));
+
+    this.quote = data[randomNum]["q"];
+    this.autor = data[randomNum]["a"];
+
+    console.log(this.quote);
+    console.log(this.autor);
+
+
+  }
 
 }
